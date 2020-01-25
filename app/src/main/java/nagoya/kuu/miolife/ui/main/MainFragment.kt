b3fb0963050c4911b1818ca94fe9fb9523aee272
fumiya-kume <fumiya.kume@hotmail.com>
2020-01-26 07:06:37 +0900

    add reload button

--- app/src/main/java/nagoya/kuu/miolife/ui/main/MainFragment.kt
+++ app/src/main/java/nagoya/kuu/miolife/ui/main/MainFragment.kt
@@ -4,20 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import nagoya.kuu.miolife.R
 import nagoya.kuu.miolife.databinding.MainFragmentBinding
+import nagoya.kuu.miolife.showToast
 import nagoya.kuu.miolife.ui.main.sim.SimDetailDialog
 import nagoya.kuu.miolife.ui.main.viewentity.SimViewEntity
 import org.koin.android.viewmodel.ext.android.viewModel
 
 class MainFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = MainFragment()
-    }
 
     private val viewModel: MainViewModel by viewModel()
 
@@ -25,6 +21,7 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        requireActivity().actionBar?.hide()
 
         val binding = MainFragmentBinding.inflate(
             layoutInflater,
@@ -51,19 +48,25 @@ class MainFragment : Fragment() {
                 when (it) {
                     is ContractListStatus.Success -> contractAdapter.submitList(it.contractList)
                     ContractListStatus.LoginRequired -> viewModel.login(this)
-                    is ContractListStatus.ErrorHappened -> {
-                        Toast.makeText(
-                            requireContext(),
-                            it.errorMessage,
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
+                    is ContractListStatus.ErrorHappened -> showToast(it.errorMessage)
+                }
+
+                binding.reloadButton.visibility = when (it) {
+                    is ContractListStatus.Success -> View.GONE
+                    ContractListStatus.LoginRequired -> View.GONE
+                    is ContractListStatus.ErrorHappened -> View.VISIBLE
                 }
             }
 
         binding.mainSwipeToRefresh.setOnRefreshListener {
             viewModel.fetchCouponData()
         }
+
+        binding
+            .reloadButton
+            .setOnClickListener {
+                viewModel.fetchCouponData()
+            }
 
         binding
             .settingButton
