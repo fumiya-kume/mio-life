fab102169f016a024a74b254cdd9a0bbd3a5e25c
fumiya-kume <fumiya.kume@hotmail.com>
2020-03-22 15:13:24 +0900

    add room index

--- app/src/main/java/nagoya/kuu/miolife/iijmio/local/HdxInfo.kt
+++ app/src/main/java/nagoya/kuu/miolife/iijmio/local/HdxInfo.kt
@@ -3,9 +3,13 @@ package nagoya.kuu.miolife.iijmio.local
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.ForeignKey
+import androidx.room.Index
 import androidx.room.PrimaryKey
 
 @Entity(
+    indices = arrayOf(
+        Index("hdd_service_code")
+    ),
     tableName = "hdx_info",
     foreignKeys = [ForeignKey(
         entity = CouponRemainEntity::class,
