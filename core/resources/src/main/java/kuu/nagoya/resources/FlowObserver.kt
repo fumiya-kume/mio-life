1864ec854e5371c89b23644751ebefa4dd6fdd30
fumiya-kume <fumiya.kume@hotmail.com>
2020-03-29 09:06:31 +0900

    add flow observer

--- core/resources/src/main/java/kuu/nagoya/resources/FlowObserver.kt
+++ core/resources/src/main/java/kuu/nagoya/resources/FlowObserver.kt
@@ -4,7 +4,15 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.runBlocking
 
+
 class FlowObserver<T>(val flow: Flow<T>) {
+
+    companion object {
+        fun <T> of(flow: Flow<T>): FlowObserver<T> {
+            return FlowObserver(flow)
+        }
+    }
+
     var values: List<T> = emptyList()
 
     init {
--- core/resources/src/test/java/kuu/nagoya/resources/FlowObserverTest.kt
+++ core/resources/src/test/java/kuu/nagoya/resources/FlowObserverTest.kt
@@ -29,4 +29,30 @@ internal class FlowObserverTest {
             )
         }
     }
+
+    @Test
+    fun ショートハンドメソッドで生成できる() {
+        listOf(
+            listOf(1),
+            listOf(0, 1, 2, 3),
+            listOf("one", "two", "three")
+        ).forEach { argument ->
+
+            val flow = flow {
+                argument.forEach { this.emit(it) }
+            }
+
+            val flowObserver = FlowObserver.of(flow)
+
+            Assert.assertEquals(
+                argument,
+                flowObserver.values
+            )
+
+            Assert.assertEquals(
+                argument.size,
+                flowObserver.values.size
+            )
+        }
+    }
 }
