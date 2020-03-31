ca5e95939ca484fe17aae5238343b3bca08860ad
fumiya-kume <fumiya.kume@hotmail.com>
2020-03-31 15:55:15 +0900

    fix ci

--- feature/auth/src/test/java/kuu/nagoya/feature/auth/domain/impl/HasAuthUsecaseImplTest.kt
+++ feature/auth/src/test/java/kuu/nagoya/feature/auth/domain/impl/HasAuthUsecaseImplTest.kt
@@ -1,11 +1,14 @@
 package kuu.nagoya.feature.auth.domain.impl
 
+import org.junit.Assert
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 
 internal class HasAuthUsecaseImplTest : Spek({
-    describe("") {
-
+    describe("aa") {
+        it("aa") {
+            Assert.assertTrue(true)
+        }
     }
 })
 
